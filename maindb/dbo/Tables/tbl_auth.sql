d2ce79d7026575d47d66695a31fa9002ea5312c5
ChheavBunHeng <bunchhengchheav@gmail.com>
2026-09-03 19:50:28 +0700

    add the database schema

---
+++ maindb/dbo/Tables/tbl_auth.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[tbl_auth] (
+    [user_id]      INT           IDENTITY (1, 1) NOT NULL,
+    [userName]     VARCHAR (50)  NOT NULL,
+    [email]        VARCHAR (255) NOT NULL,
+    [userPassword] VARCHAR (255) NOT NULL,
+    [is_active]    BIT           DEFAULT ((1)) NOT NULL,
+    [created_dt]   DATETIME      DEFAULT (getdate()) NOT NULL,
+    [updated_dt]   DATETIME      DEFAULT (getdate()) NOT NULL,
+    PRIMARY KEY CLUSTERED ([user_id] ASC)
+);
+
---
+++ maindb/dbo/Tables/tbl_notes.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[tbl_notes] (
+    [note_id]    INT           IDENTITY (1, 1) NOT NULL,
+    [title]      VARCHAR (100) NOT NULL,
+    [content]    TEXT          NULL,
+    [created_dt] DATETIME      DEFAULT (getdate()) NOT NULL,
+    [updated_dt] DATETIME      DEFAULT (getdate()) NOT NULL,
+    [user_id]    INT           NOT NULL,
+    PRIMARY KEY CLUSTERED ([note_id] ASC),
+    CONSTRAINT [FK_tbl_notes_tbl_auth] FOREIGN KEY ([user_id]) REFERENCES [dbo].[tbl_auth] ([user_id]) ON DELETE CASCADE
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_tbl_notes_user_id]
+    ON [dbo].[tbl_notes]([user_id] ASC);
+
